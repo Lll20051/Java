593724bb9488a870cbb0dfdbcf0f260ccb1567ac
Lll20051 <149168430+Lll20051@users.noreply.github.com>
2023-10-30 18:17:42 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+    public static void main(String[] args)
+        {
+            int x;
+            for(x=1;x<=8;x++)
+            {
+                System.out.println("x*x="+(x+x*x));
+            }
+        }
+    }
