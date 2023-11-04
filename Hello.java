1534b36b50d4e504e3d070e2232af92b08240855
Lll20051 <149168430+Lll20051@users.noreply.github.com>
2023-11-04 19:21:52 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,10 +1,25 @@
-public class Hello{
-    public static void main(String[] args)
-        {
-            int x;
-            for(x=1;x<=8;x++)
-            {
-                System.out.println("x*x="+(x+x*x));
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int a,b=0;
+        if(x<0){
+            System.out.println("false");
+        }
+        else{
+            a=x;
+            while(x!=0){
+                b=b*10+x%10;
+                x=x/10;
+            }
+            if(a==b){
+                System.out.println("true");
+            }
+            else{
+                System.out.println("false");
             }
         }
     }
+}
+
